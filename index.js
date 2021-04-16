f5d1cb9e87140d7ca62f05337e4a3b8e0951fb3f
tchartchke <chartai.kl@gmail.com>
2021-04-16 11:22:22 -0500

    commit tree e57bb72b9163ce260dfcf663a89b357cd05abc63

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+const address = 42
+
+function distanceFromHqInBlocks(num){
+  return Math.abs(num - address)
+}
+
+function distanceFromHqInFeet(num){
+  return distanceFromHqInBlocks(num)*264
+}
+
+function distanceTravelledInFeet(start, end){
+  return Math.abs(start - end)*264
+}
+
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination)
+  switch (true) {
+    case distance <= 400 : return 0;
+    case distance > 2500 : return 'cannot travel that far';
+    case distance > 2000 : return 25;
+    default : return (distance-400)*0.02
+  }
+}
